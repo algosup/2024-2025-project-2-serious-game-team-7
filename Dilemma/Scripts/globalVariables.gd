72a0d957e051997449cbbc661e638375a1ec398c
ClementineCurel <78617457+Clementine951@users.noreply.github.com>
2024-12-12 15:01:11 +0100

    Revert "update gameover"
    This reverts commit 2a35ed8fbdbb340dc41c6ed54eede868aef80b1a.

--- Dilemma/Scripts/globalVariables.gd
+++ Dilemma/Scripts/globalVariables.gd
@@ -13,21 +13,7 @@ var rndMoney: int = 0
 var currentTurn: int = 0
 
 # RnD levels
+
 const MAX_RND_LEVEL = 5
 var rndLevels: Array = [0, 0, 0, 0, 0, 0]  # Initial levels for all sliders
 var rndCursors: Array = [0, 0, 0, 0, 0, 0]  # Cursor positions for all sliders
-
-# Death checking
-var isDead: bool = false
-
-func _process(delta: float) -> void:
-    # Update the money and temperature labels
-    $Money.value = currentMoney
-    $Temperature.value = currentTemperature
-
-    # Check if the player is dead
-    if currentMoney <= 0 or currentTemperature >= 30:
-        isDead = true
-        print("Game Over!")
-        # Show game over screen
-        get_tree().change_scene("res://Scenes/mainMenu.tscn")
