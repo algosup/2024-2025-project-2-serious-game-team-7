a2155a707a7133618d70a09bad2f522a701b6c4a
PaulNowak36 <91249965+PaulNowak36@users.noreply.github.com>
2024-12-12 14:34:58 +0100

    pause menu + intro

--- Dilemma/Scripts/control.gd
+++ Dilemma/Scripts/control.gd
@@ -1,14 +1,19 @@
 extends Control
 
+@onready var pause_menu = $stopMenu
+var paused = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pause_menu.hide() # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$Money.value = GlobalVariables.currentMoney
-	$Temperature.value = GlobalVariables.currentTemperature
-
+	$Temperature.value = GlobalVariables.currentTemperature	
+	if Input.is_action_just_pressed("TogglePause"):
+		pauseMenu()
+		
 func _updateMoneyAndTemperature(lawMoney: int, lawTemperature: int) -> void:
 	GlobalVariables.currentMoney += lawMoney
 	GlobalVariables.currentTemperature += lawTemperature
@@ -21,3 +26,12 @@ func _updateMoneyAndTemperature(lawMoney: int, lawTemperature: int) -> void:
 		GlobalVariables.currentMoney = 100
 	if GlobalVariables.currentTemperature > 100:
 		GlobalVariables.currentTemperature = 100
+		
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 1
+	paused = !paused
